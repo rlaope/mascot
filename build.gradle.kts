eecb6f683a65b975ff3170a0f65ad8b232f993ce
rlaope <piyrw9754@gmail.com>
2025-12-03 17:54:09 +0900

    plugin set

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.9.24"
+    id("org.jetbrains.intellij") version "1.17.3"
+}
+
+group = "com.khope.cornermascot"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+intellij {
+    version.set("2024.1")
+    type.set("IC")
+    plugins.set(emptyList())
+}
+
+tasks {
+    patchPluginXml {
+        sinceBuild.set("241")
+        untilBuild.set("251.*")
+    }
+
+    compileKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+    compileTestKotlin {
+        kotlinOptions.jvmTarget = "17"
+    }
+}
+
+publishPlugin {
+    token.set(System.getenv("JB_PLUGIN_TOKEN"))
+}
